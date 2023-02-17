9c9110a8093f71277c0176a33c0f39b6c443183c
cmdprm <2badhermit@gmail.com>
2023-02-18 02:03:23 +0300

    First commit

---
+++ CryptoRates-Marathon/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CryptoRates-Marathon
+//
+//  Created by User on 18.02.2023.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]?) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        window?.backgroundColor = .systemBackground
+        window?.rootViewController = ViewController()
+        
+        return true
+    }
+
+}
+
---
+++ CryptoRates-Marathon/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  CryptoRates-Marathon
+//
+//  Created by User on 18.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
